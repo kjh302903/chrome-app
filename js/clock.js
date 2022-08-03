40bb48772bae10310ec597e6c2b5c8490042424a
kjh302903 <kjh302903@gmail.com>
2022-08-03 17:03:02 +0900

    시간이 세로로 보이던것 수정

--- js/clock.js
+++ js/clock.js
@@ -2,9 +2,10 @@ const clock = document.querySelector("#clock");
 
 function getClock() {
     const date = new Date();
-    clock.innerText = `${date.getHours().toString().padStart(2,"0")}:
-    ${date.getMinutes().toString().padStart(2,"0")}:
-    ${date.getSeconds().toString().padStart(2,"0")}`;
+    const hours = date.getHours().toString().padStart(2,"0");
+    const minutes = date.getMinutes().toString().padStart(2,"0");
+    const seconds = date.getSeconds().toString().padStart(2,"0");
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 getClock();
