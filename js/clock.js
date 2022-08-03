58ad29eca8da3f742e2650750bb1353b1c87522b
kjh302903 <kjh302903@gmail.com>
2022-08-03 16:50:58 +0900

    add clock

--- js/clock.js
+++ js/clock.js
@@ -1,1 +1,11 @@
 const clock = document.querySelector("#clock");
+
+function getClock() {
+    const date = new Date();
+    clock.innerText = `${date.getHours().toString().padStart(2,"0")}:
+    ${date.getMinutes().toString().padStart(2,"0")}:
+    ${date.getSeconds().toString().padStart(2,"0")}`;
+}
+
+getClock();
+setInterval(getClock, 1000);
