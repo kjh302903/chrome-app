9fdb740925cd281100e0b006e4e78a70bec29662
kjh302903 <kjh302903@gmail.com>
2022-08-04 14:46:24 +0900

    add deleteBtn

--- js/todolist.js
+++ js/todolist.js
@@ -2,11 +2,20 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector("#todo-list");
 
+function deleteToDo(event) {
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 function paintToDO(inputValue) {
     const li = document.createElement("li");
     const span = document.createElement("span");
-    li.appendChild(span);
     span.innerText = inputValue;
+    const btn = document.createElement("button");
+    btn.innerText = "X";
+    btn.addEventListener("click", deleteToDo);
+    li.appendChild(span);
+    li.appendChild(btn);
     toDoList.appendChild(li);
 }
 
